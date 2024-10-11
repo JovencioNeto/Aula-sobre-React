07ca8bb23de84a25d15458b9a06dcfdd86a97fa2
Jovêncio Neto <151071159+JovencioNeto@users.noreply.github.com>
2024-10-11 08:28:52 -0300

    Add files via upload

---
+++ script.jsx
@@ -0,0 +1,13 @@
+// Criando um componente. Component é uma função no javascript
+// Deve ser escrito em pascal case, por boa prática
+
+function Button(){
+    return (
+        // Detalhes
+        <div className="container">
+            <h1>Meu Título</h1>
+            <p>Meu parágrafo</p>
+            <button>Meu Botão</button>
+        </div>
+    )
+}
